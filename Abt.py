17ef19d16961ec4ce3f93f809f4852b465687fca
Arhaam216 <arhaammuhammad186@gmail.com>
2024-09-29 14:07:20 +0400

    Abt.py
    By this python file you can learn about me, my social id and free fire I'd give away.

---
+++ Abt.py
@@ -0,0 +1,30 @@
+def main():
+  while True:
+    print("Menu:")
+    print("1. About Me")
+    print("2. my social")
+    print("3. free fire accounts")
+    print("4. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+      about_me = input("Enter your information: ")
+      print("Your information:", hello my name is arhaam)
+    elif choice == 2:
+      print("insta: @arhaam__x__
+      Facebook:https://www.facebook.com/profile.php?id=61562307864110
+      ")
+    elif choice == 3:
+      print(" 1. id: Facebook:fefife8379@aiworldx.com
+      pss:shonigauz12
+      2. id: Facebook:lamelif394@z3rb.com
+      pss:lamelif12")
+    elif choice == 4:
+      print("Exiting...")
+      break
+    else:
+      print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+  main()
