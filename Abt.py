8fdd6b6d0c0a046b06f0a1f4903817888b3a79ef
Arhaam216 <arhaammuhammad186@gmail.com>
2024-10-04 15:02:25 +0400

    cool designs
    here are some cool python designs

--- Abt.py
+++ Abt.py
@@ -1,30 +1,55 @@
+import turtle
+
 def main():
-  while True:
-    print("Menu:")
-    print("1. About Me")
-    print("2. my social")
-    print("3. free fire accounts")
-    print("4. Exit")
+    print("\n\tWelcome to Cool Designs!\n")
+    print("\t1. Designs")
+    print("\t2. Source Codes")
+    print("\t3. Exit\n")
 
-    choice = int(input("Enter your choice: "))
+    choice = input("\tEnter your choice (1-3): ")
 
-    if choice == 1:
-      about_me = input("Enter your information: ")
-      print("Your information:", hello my name is arhaam)
-    elif choice == 2:
-      print("insta: @arhaam__x__
-      Facebook:https://www.facebook.com/profile.php?id=61562307864110
-      ")
-    elif choice == 3:
-      print(" 1. id: Facebook:fefife8379@aiworldx.com
-      pss:shonigauz12
-      2. id: Facebook:lamelif394@z3rb.com
-      pss:lamelif12")
-    elif choice == 4:
-      print("Exiting...")
-      break
+    if choice == "1":
+        show_designs()
+    elif choice == "2":
+        show_source_codes()
+    elif choice == "3":
+        print("\n\tExiting...\n")
     else:
-      print("Invalid choice. Please try again.")
+        print("\n\tInvalid choice. Please try again.\n")
+
+def show_designs():
+    turtle.speed(0)
+    turtle.bgcolor("black")
+
+    # Design 1: Spiral
+    turtle.color("blue")
+    for i in range(100):
+        turtle.forward(i)
+        turtle.right(144)
+
+    # Design 2: Star
+    turtle.penup()
+    turtle.goto(200, 0)
+    turtle.pendown()
+    turtle.color("red")
+    for i in range(5):
+        turtle.forward(100)
+        turtle.right(144)
+
+    # Design 3: Circle
+    turtle.penup()
+    turtle.goto(-200, 0)
+    turtle.pendown()
+    turtle.color("green")
+    turtle.circle(100)
+
+    # ... (more designs)
+
+    turtle.done()
+
+def show_source_codes():
+    print("\n\tSource codes for the designs will be displayed here.\n")
+    # ... (display source codes for each design)
 
 if __name__ == "__main__":
-  main()
+    main()
